f453205d7eddc8530a85ec392d8c16fc000e6341
Pierre-Chanel Gauthier <pierre-chanel.gauthier@fr.airtag.com>
2015-07-24 14:51:52 +0200

    Creating distribution parameters file

---
+++ src/KmeCnin/Bundle/FumbleManiaBundle/DataFixtures/ORM/UsersFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KmeCnin\Bundle\FumbleManiaBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use KmeCnin\Bundle\FumbleManiaBundle\Entity\User;
+
+class UsersFixture extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userTest = new User();
+        $userTest->setUsername('test');
+        $userTest->setPassword('test');
+
+        $manager->persist($userTest);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
